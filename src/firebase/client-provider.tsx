751ff39d67de5c77e919255214570303aaf62d2b
Andrew Bale RCNX <andrew.bale@rcnx.io>
2025-11-22 04:39:41 +0000

    OK - we're not advancing here.   Typically when you enter this kind of l

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -4,5 +4,6 @@ import { initializeFirebase } from '.';
 
 // Initalizes and provides the firebase app on the client.
 export function FirebaseClientProvider({children}: {children: React.ReactNode}) {
-  return <FirebaseProvider {...initializeFirebase()}>{children}</FirebaseProvider>;
+  const { firebaseApp, firestore, auth, storage } = initializeFirebase();
+  return <FirebaseProvider value={{firebaseApp, firestore, auth, storage}}>{children}</FirebaseProvider>;
 }
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,7 @@
 import {initializeApp, getApp, getApps, FirebaseApp} from 'firebase/app';
 import {getFirestore, Firestore} from 'firebase/firestore';
 import {getAuth, Auth} from 'firebase/auth';
+import { getStorage, FirebaseStorage } from 'firebase/storage';
 import {firebaseConfig} from './config';
 
 export * from './provider';
@@ -11,11 +12,13 @@ export function initializeFirebase(): {
   firebaseApp: FirebaseApp;
   firestore: Firestore;
   auth: Auth;
+  storage: FirebaseStorage;
 } {
   const firebaseApp = !getApps().length
     ? initializeApp(firebaseConfig)
     : getApp();
   const firestore = getFirestore(firebaseApp);
   const auth = getAuth(firebaseApp);
-  return {firebaseApp, firestore, auth};
+  const storage = getStorage(firebaseApp);
+  return {firebaseApp, firestore, auth, storage};
 }
